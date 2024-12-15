a0cd69657ada0adf23389878775f3e4aca379ca2
sanfarish <farisfalah@gmail.com>
2024-12-15 18:23:09 +0700

    add standard.js style guide

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
 # Bookshelf API
-* This repo is made to record my project for dicoding.com class on "Belajar Back-End Pemula dengan JavaScript" as final assessment to get my certificate.
+[![js-standard-style](https://cdn.rawgit.com/standard/standard/master/badge.svg)](http://standardjs.com)
+* This repo is made to record my project for dicoding.com class on "Belajar Back-End Pemula dengan JavaScript" as final assessment to graduate and get a certificate.
+* This project is a simple bookshelf API that can be used to manage books in a library.
+* This RESTful API has several endpoints to perform CRUD operations on books.
+* This project uses standard/standard style guide.
+## Tech used:
+* Node.js
+* Hapi
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import importPlugin from 'eslint-plugin-import'
+import nodePlugin from 'eslint-plugin-n'
+import pluginPromise from 'eslint-plugin-promise'
+import config from 'eslint-config-standard'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...config.globals, ...config.env },
+      parserOptions: config.parserOptions
+    },
+    plugins: {
+      standard: config.plugins,
+      n: nodePlugin,
+      promise: pluginPromise,
+      import: importPlugin
+    },
+    rules: config.rules
+  }
+]
